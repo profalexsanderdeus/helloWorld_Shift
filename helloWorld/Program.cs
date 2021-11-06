b94b1b2d33dbbaea439f666a86d22ed65cafbf99
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-11-06 09:56:36 -0300

    aula sobre concatenar - hello world

---
+++ helloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFilme,sala,hora,data;
+
+            Console.WriteLine("Insira o nome do filme:");
+            nomeFilme = Console.ReadLine();
+
+            Console.WriteLine("Insira a sala:");
+            sala = Console.ReadLine();
+
+            Console.WriteLine("Qual o horário do filme");
+            hora = Console.ReadLine();
+
+            Console.WriteLine("Data:");
+            data = Console.ReadLine();
+
+            Console.WriteLine("Seu ingresso para o filme {0}, sala {1} no dia {2} às {3}",
+                nomeFilme,sala,data,hora);
+        }
+    }
+}
